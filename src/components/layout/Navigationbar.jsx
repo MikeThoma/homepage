4c1bab73ee83ac6b9a040d323dae81b6afc322bf
Mike Thoma <mike.thoma@hotmail.de>
2023-01-31 19:44:06 +0100

    Remove extra margin of navbar brand

--- src/components/layout/Navigationbar.jsx
+++ src/components/layout/Navigationbar.jsx
@@ -8,7 +8,7 @@ import { Link } from "react-router-dom";
 
 const Navigationbar = () => {
     return <Navbar expand='sm'>
-        <Navbar.Brand style={{ paddingLeft: '30px' }}><Link to='/' style={{ paddingLeft: '30px', textDecoration: 'none', color: 'black' }}>Mike Thoma</Link></Navbar.Brand>
+        <Navbar.Brand><Link to='/' style={{ paddingLeft: '30px', textDecoration: 'none', color: 'black' }}>Mike Thoma</Link></Navbar.Brand>
         <Navbar.Toggle className='navbar-toggle' aria-controls='navbarScroll' style={{ marginRight: '20px', border: 'none' }} />
         <Navbar.Collapse className='justify-content-end'>
             <Nav className="align-items-center navbar-items-container">
